f61d7efe3daafd3c503df4770b627391cfc82fc9
Seth Lopez <slopez@bignerdranch.com>
2022-09-01 16:34:29 -0400

    Add a button to roll the dice

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -16,14 +16,42 @@
 
 package com.example.diceroller;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * This method is called when the Activity is created.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the Button in the layout
+        Button rollButton = findViewById(R.id.button);
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener(v -> rollDice());
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private void rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        Dice dice = new Dice(6);
+        int diceRoll = dice.roll();
+
+        // Update the screen with the dice roll
+        TextView resultTextView = findViewById(R.id.textView);
+        resultTextView.setText(String.valueOf(diceRoll));
     }
 }
