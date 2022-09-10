5382f8068c48eeaee51ad24be03b6df188c7d21f
Seth Lopez <slopez@bignerdranch.com>
2022-09-09 20:10:43 -0400

    Add a unit test for the dice roll logic

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.java
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.java
@@ -1,17 +1,13 @@
 package com.example.diceroller;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void generates_number() {
+        Dice dice = new Dice(6);
+        int diceRoll = dice.roll();
+        assertTrue("The value of diceRoll was not between 1 and 6", diceRoll > 0 && diceRoll < 7);
     }
 }
